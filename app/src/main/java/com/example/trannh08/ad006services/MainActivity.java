ca3175af011e37d5ebb29f0b2c3b009d4b4a2b19
Koichi <koichisojiro@live.com>
2016-08-25 15:39:49 +0700

    finished

--- app/src/main/java/com/example/trannh08/ad006services/MainActivity.java
+++ app/src/main/java/com/example/trannh08/ad006services/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.trannh08.ad006services;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.example.trannh08.ad006services.utils.MyService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startMyService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    public void stopMyService (View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 }
